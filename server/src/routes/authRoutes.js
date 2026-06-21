684d90aa9d36677452d98f325fed472a7075ad4d
Mantosh <mahiraval000@gmail.com>
2026-06-22 04:13:26 +0530

    change file name into camelCase

---
+++ server/src/routes/authRoutes.js
@@ -0,0 +1,13 @@
+import { Router } from 'express'
+import { register, login, logout, getMe, googleCallback } from '../controllers/authController.js'
+import { protect } from '../middlewares/authMiddleware.js'
+
+const router = Router()
+
+router.post('/register', register)
+router.post('/login', login)
+router.post('/logout', logout)
+router.post('/google/callback', googleCallback)
+router.get('/me', protect, getMe)
+
+export default router
