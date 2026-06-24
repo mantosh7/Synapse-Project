d8c44a3c5fd0f70b00c61fcb2457c12676cc1c95
Mantosh <mahiraval000@gmail.com>
2026-06-24 16:09:16 +0530

    fix: update SameSite policy for cross-origin auth

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -3,7 +3,7 @@ import { registerUser, loginUser, getUserById, googleLogin } from '../services/a
 const cookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === 'production',
-  sameSite: 'strict',
+  sameSite: 'none',
   maxAge: 7 * 24 * 60 * 60 * 1000
 }
 
